de105c494c6168197b8f9fc189376b7f51e8103b
Juha Nieminen <101553688+ieponi@users.noreply.github.com>
2024-08-09 16:54:32 +0300

    Update app.js

--- RESTAPI2/app.js
+++ RESTAPI2/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'seppohauki',
+    password: 'omasalasana',
     database: 'tietokanta',
     port: 3306
 });
